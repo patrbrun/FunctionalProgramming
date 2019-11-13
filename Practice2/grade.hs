c13eebf4d5e85f87152bf68fac9a7322b7a29594
Patricia Bruno <paaat29@gmail.com>
2019-11-13 13:04:53 +0200

    Added Grade assignment

--- Practice2/grade.hs
+++ Practice2/grade.hs
@@ -1,2 +1,14 @@
 -- Write a program with guards in haskell that can provide a grade with the following scale: 
 --   80-90 – A+, 70-79 – A, 60-69 – B, 50 – 59 – C and less than 50 is fail.
+
+grade :: (Num a, Ord a) => a -> [Char]
+grade score
+   | score >= 80 = "A+"
+   | score >= 70 = "A"
+   | score >= 60 = "B"
+   | score >= 50 = "C"
+   | otherwise   = "Fail"
+   
+main = do
+  putStrLn "Your score was 83, then your grade is:"  
+  print (grade 83)
