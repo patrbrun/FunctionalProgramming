c9e16653ec21deac5ed28d1fb949f6042f96d4b6
Patricia Bruno <paaat29@gmail.com>
2019-12-16 11:21:57 +0200

    Fix odd number assignment

--- Assignment2/sumOddNumbers.hs
+++ Assignment2/sumOddNumbers.hs
@@ -1,5 +1,6 @@
 -- Write a program which calculates the sum of a ll odd numbers in a given range.
 
 -- Done in GHCI
+
 -- Sum all the off numbers in a range of 0 to 15.
-sum (takeWhile (<15) (filter odd (map (^2) [1..])))
+sum (takeWhile (<15) (filter odd [1..]))
