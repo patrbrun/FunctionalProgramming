f33859796eccb3d6c95aa4f40c5c03ef30889cb3
Patricia Bruno <paaat29@gmail.com>
2019-12-17 15:47:13 +0200

    Add practice 7

--- Practice7/filter.hs
+++ Practice7/filter.hs
@@ -1,2 +1,10 @@
 -- Write a program using Filter where there is a list of years from 1990 until the current year.
 -- Find out the first year in the list which is a leap year.
+
+largestDivisible :: (Integral a) => a  
+largestDivisible = head (filter p [1990..2019])  
+  where p x = x `mod` 4 == 0
+
+main = do
+  putStrLn "The first leap year since 1990 is:"
+  print $ largestDivisible
--- Practice7/scans.hs
+++ Practice7/scans.hs
@@ -1,1 +1,5 @@
--- Write a program using Scans where input is an umber and the output is the multiplication of that number with 5.
+-- Write a program using Scans where input is a number and the output is the multiplication of that number with 5.
+
+main = do  
+    putStrLn "The number chosen is 43. What is the multiplication of 43 by 5?"  
+    print (scanl(*) 43 [5])
